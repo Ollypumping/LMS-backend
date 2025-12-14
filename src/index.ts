fcaf5c102f90bb2113dff9838d87e8012690918d
David <ajiboladavid0963@gmail.com>
2025-12-14 20:35:31 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { env } from "./config/env";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Hello world");
+});
+
+const PORT = env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
